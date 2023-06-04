b65968f9c63738bc9ed317c84d57793cf8e2b368
Matt Aitken <matt@mattaitken.com>
2023-06-04 17:03:40 +0100

    Removed unused components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,13 @@
 import '#/styles/globals.css';
-import { AddressBar } from '#/ui/address-bar';
-import Byline from '#/ui/byline';
-import { GlobalNav } from '#/ui/global-nav';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: {
-    default: 'Next.js App Router',
-    template: '%s | Next.js App Router',
+    default: 'Company Icons by Trigger.dev',
+    template: '%s | By Trigger.dev',
   },
   description:
-    'A playground to explore new Next.js App Router features such as nested layouts, instant loading states, streaming, and component level data fetching.',
+    'Easily render Company icons in your React app. Made by Trigger.dev',
 };
 
 export default function RootLayout({
@@ -19,22 +16,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="[color-scheme:dark]">
-      <body className="bg-gray-1100 overflow-y-scroll bg-[url('/grid.svg')] pb-36">
-        <GlobalNav />
-
+    <html lang="en">
+      <body className="overflow-y-scroll bg-slate-900 bg-[url('/grid.svg')] pb-36">
         <div className="lg:pl-72">
-          <div className="mx-auto max-w-4xl space-y-8 px-2 pt-20 lg:py-8 lg:px-8">
-            <div className="rounded-lg bg-vc-border-gradient p-px shadow-lg shadow-black/20">
-              <div className="rounded-lg bg-black">
-                <AddressBar />
-              </div>
+          <div className="mx-auto max-w-4xl space-y-8 px-2 pt-20 lg:px-8 lg:py-8">
+            <div className="bg-vc-border-gradient rounded-lg p-px shadow-lg shadow-black/20">
+              <div className="rounded-lg bg-black"></div>
             </div>
 
-            <div className="rounded-lg bg-vc-border-gradient p-px shadow-lg shadow-black/20">
+            <div className="bg-vc-border-gradient rounded-lg p-px shadow-lg shadow-black/20">
               <div className="rounded-lg bg-black p-3.5 lg:p-6">{children}</div>
             </div>
-            <Byline className="fixed sm:hidden" />
           </div>
         </div>
       </body>
