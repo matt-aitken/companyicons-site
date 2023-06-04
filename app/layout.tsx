4ef64ae953e8bcd8b54f443c04454bc2a6ad0fb0
Matt Aitken <matt@mattaitken.com>
2023-06-04 17:45:16 +0100

    Using the package, display the icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,19 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="overflow-y-scroll bg-slate-900 bg-[url('/grid.svg')] pb-36">
-        <div className="lg:pl-72">
-          <div className="mx-auto max-w-4xl space-y-8 px-2 pt-20 lg:px-8 lg:py-8">
-            <div className="bg-vc-border-gradient rounded-lg p-px shadow-lg shadow-black/20">
-              <div className="rounded-lg bg-black"></div>
-            </div>
-
-            <div className="bg-vc-border-gradient rounded-lg p-px shadow-lg shadow-black/20">
-              <div className="rounded-lg bg-black p-3.5 lg:p-6">{children}</div>
-            </div>
-          </div>
-        </div>
-      </body>
+      <body className="overflow-y-scroll bg-slate-900 ">{children}</body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,53 @@
+import { CompanyIcon, icons } from '@trigger.dev/companyicons';
+import { Fragment } from 'react';
+import clsx from 'clsx';
+
 export default function Page() {
-  return <h1>Hello, Home page!</h1>;
+  return (
+    <main className="flex justify-center">
+      <div className="w-full max-w-5xl">
+        <h1 className="text-3xl text-slate-200">Company Icons</h1>
+        <div className="flex flex-wrap gap-4">
+          {Object.entries(icons).map(([name, icon]) => {
+            if ('all' in icon) {
+              return <Icon key={name} name={name} />;
+            }
+
+            return (
+              <Fragment key={name}>
+                <Icon name={name} variant="light" />
+                <Icon name={name} variant="dark" />
+              </Fragment>
+            );
+          })}
+        </div>
+      </div>
+    </main>
+  );
+}
+
+function Icon({ name, variant }: { name: string; variant?: 'light' | 'dark' }) {
+  return (
+    <div className="flex flex-col items-center justify-center gap-2">
+      <div
+        className={clsx(
+          'relative flex h-24 w-24 flex-col items-center justify-center rounded-md border border-slate-700',
+          variant === 'dark' && 'bg-slate-200',
+        )}
+      >
+        <CompanyIcon name={name} variant={variant} className="block h-6 w-6" />
+        {variant && (
+          <p
+            className={clsx(
+              'absolute bottom-1 text-sm',
+              variant === 'dark' ? 'text-slate-800' : 'text-slate-200',
+            )}
+          >
+            {variant}
+          </p>
+        )}
+      </div>
+      <p className="text-white">{name}</p>
+    </div>
+  );
 }
